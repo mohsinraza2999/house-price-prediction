11e71604c1e23f4101ad9d35295e53fd2382b5b6
mohsinraza2999 <mohsinraza2999@gmail.com>
2026-01-23 09:31:25 -0800

    add url in test file

--- tests/test_api_routes.py
+++ tests/test_api_routes.py
@@ -6,14 +6,14 @@ import requests
 
 class TestRoutes(unittest.TestCase):
     def setUp(self)-> None:
-        self.url="http://127.0.0.1:8000/"
+        self.url="http://localhost:8000"
         self.data={"val1":0.02729,	"val2":0.0,	"val3":8.07,	"val4":0.0,
           "val5":0.469,	"val6":7.185,	"val7":61.1,	"val8":4.9671,	"val9":2.0,	"val10":242.0,	"val11":17.8,
           "val12":392.83,	"val13":4.03}
 
     def test_health(self):
 
-        response=requests.get(self.url)
+        response=requests.get(f"{self.url}/")
         self.assertEqual(response.status_code, 200)
         data = response.json()
         self.assertIsInstance(data, dict)
